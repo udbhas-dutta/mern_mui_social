584ea50528ae50e7b4181022d1a1fec44a0b8dbc
Udbhas Dutta <udbhasdutta@gmail.com>
2024-06-24 20:50:36 +0530

    added functionality for user registration and login, post creation and likes

---
+++ backend/models/Post.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        picturePath: {
+            type: String,
+        },
+        userPicturePath: {
+            type: String,
+        },
+        likes: {
+            type: Map,
+            of: Boolean,
+        },
+        comments: {
+            type: Array,
+            defalut: []
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
